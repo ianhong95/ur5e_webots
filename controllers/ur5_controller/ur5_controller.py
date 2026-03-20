fb6e9ef63fc9e9812f2ad2e1a1a84efc4c37afc3
Ian Hong <ianqyhong@gmail.com>
2026-03-19 22:28:49 -0400

    Added support for pure translation and pure rotation, but go_to_position needs to be reworked to allow motions to finish before continuing.

--- controllers/ur5_controller/ur5_controller.py
+++ controllers/ur5_controller/ur5_controller.py
@@ -24,9 +24,9 @@ class UR5Controller(Robot):
 
     TEST_TARGETS = {
         'TEST': np.array([
-            [1.000, 0.000, 0.000, 400.000],
+            [1.000, 0.000, 0.000, 200.000],
             [0.000, -1.000, 0.000, 133.000],
-            [0.000, 0.000, -1.000, 250.000],
+            [0.000, 0.000, -1.000, 200.000],
             [0.000, 0.000, 0.000, 1.000]
         ]),
         'TEST2': np.array([
@@ -88,36 +88,60 @@ class UR5Controller(Robot):
             self.joint_angles[joint.idx] = sensor.getValue()
         
         # Debug printing
-        joint_angles = [f'{angle:.5f}' for angle in self.joint_angles]
+        # joint_angles = [f'{angle:.5f}' for angle in self.joint_angles]
 
-    def go_to_position(self, target_tf: np.ndarray):
+    def go_to_position(self, target_tf: np.ndarray, linear_speed: float = 200.0, angular_speed: float = 1.0):
         """
         Use inverse kinematics to move to a target TF.
-        """
-        solution_converged = False
-        iteration = 0
-        current_joint_angles = self.joint_angles.copy()
-        target_joint_angles = [0.0] * PhysicalParams.NUM_JOINTS
 
-        while not solution_converged:
-            angles_increment, rot_error, trans_error = self.k.inv_kinematics(target_tf, current_joint_angles)
+        Linear speed is 100 mm/s by default.
+        """
+        self.update_joint_angles()
+        self.step(self.TIMESTEP)
+
+        target_joint_angles = self.joint_angles.copy()
+        _, initial_pose = self.k.body_forward_kinematics(self.joint_angles)
+
+        # Solve inverse kinematics numerically using Newton-Raphson
+        for i in range(IntConstants.MAX_ITERATIONS):
+
+            # Set the initial guess to the "target_joint_angles" which are initially the current joint angles
+            delta_theta, rot_error, trans_error = self.k.inv_kinematics(target_tf, target_joint_angles)
+
             if rot_error > FloatConstants.IK_ERROR_THRESHOLD or trans_error > FloatConstants.IK_ERROR_THRESHOLD:
-                for angle_increment, (joint, motor) in zip(angles_increment, self.motors.items()):
-                    # self.joint_angles[joint.idx] += FloatConstants.DAMPING_FACTOR * angle_increment.item()
-                    current_joint_angles[joint.idx] += FloatConstants.DAMPING_FACTOR * angle_increment.item()
-                iteration += 1
+                for idx, angle_increment in enumerate(delta_theta):
+                    target_joint_angles[idx] += FloatConstants.DAMPING_FACTOR * angle_increment.item()
             else:
-                solution_converged = True
-                target_joint_angles = current_joint_angles
                 break
         
-        steps = []
+        # Calculate linear distance between target and current pose
+        linear_dist = abs(np.linalg.norm(target_tf[:3, 3] - initial_pose[:3, 3]))
 
+        # Calculate number of timesteps required to move at the provided speed.
+        mm_per_step = linear_speed * IntConstants.TIMESTEP / 1000
+
+        num_steps_linear = np.clip(int(linear_dist / mm_per_step), 1, 200)
+
+        # Extract rotation matrices
+        R_initial = initial_pose[:3, :3]
+        R_target = target_tf[:3, :3]
+        
+        # Calculate relative rotation angle
+        R_rel = R_initial.T @ R_target
+        acos_arg = np.clip((np.trace(R_rel) - 1.0) / 2.0, -1.0, 1.0)
+        angular_dist = abs(np.arccos(acos_arg)) # Radian difference
+        
+        rad_per_step = angular_speed * IntConstants.TIMESTEP / 1000.0
+        num_steps_angular = int(angular_dist / rad_per_step) if rad_per_step > 0 else 0
+
+        # Put a cap on the number of steps
+        num_steps = np.clip(max(num_steps_linear, num_steps_angular), 1, 200)
+        
+        # Calculate the angle steps required by each joint at each timestep
+        angles_per_step = []
         for joint, motor in self.motors.items():
             angle_diff = target_joint_angles[joint.idx] - self.joint_angles[joint.idx]
-            max_angle_step = FloatConstants.MAX_VELOCITY * self.TIMESTEP / 1000
-            step = np.clip(angle_diff, -max_angle_step, max_angle_step)
-            steps.append(step)
+            angles_per_step.append(angle_diff / num_steps)    # rad/step
 
         while self.step(self.TIMESTEP) != -1:
             self.target_reached = True
@@ -126,7 +150,7 @@ class UR5Controller(Robot):
                 angle_error = abs(target_joint_angles[joint.idx] - self.joint_angles[joint.idx])
 
                 if angle_error > FloatConstants.IK_ERROR_THRESHOLD:
-                    self.joint_angles[joint.idx] += steps[joint.idx]
+                    self.joint_angles[joint.idx] += angles_per_step[joint.idx]
                     motor.setPosition(self.joint_angles[joint.idx])
                     print(f'Target not reached. Error: {angle_error}')
                     self.target_reached = False
@@ -161,15 +185,78 @@ class UR5Controller(Robot):
                 break
 
     def move_x(self, x_target: float):
-        pass
+        target_coords = (x_target, 0, 0)
+        _, current_tf = self.k.body_forward_kinematics(self.joint_angles)
+
+        target_tf = self.k.rel_trans_xyz(target_coords, current_tf)
+
+        self.go_to_position(target_tf)
+
+        return self
+    
+    def move_y(self, y_target: float):
+        target_coords = (0, y_target, 0)
+        self.update_joint_angles()
+        self.step(self.TIMESTEP)
+        _, current_tf = self.k.body_forward_kinematics(self.joint_angles)
+
+        target_tf = self.k.rel_trans_xyz(target_coords, current_tf)
+
+        self.go_to_position(target_tf)
+
+        return self
+    
+    def move_z(self, z_target: float):
+        target_coords = (0, 0, z_target)
+        _, current_tf = self.k.body_forward_kinematics(self.joint_angles)
+
+        target_tf = self.k.rel_trans_xyz(target_coords, current_tf)
+
+        self.go_to_position(target_tf)
+
+        return self
+    
+    def rot_x(self, theta: float):
+        _, current_tf = self.k.body_forward_kinematics(self.joint_angles)
+
+        target_tf = self.k.rot_x(theta, current_tf)
+
+        self.go_to_position(target_tf)
+
+        return self
+    
+    def rot_y(self, theta: float):
+        _, current_tf = self.k.body_forward_kinematics(self.joint_angles)
+
+        target_tf = self.k.rot_y(theta, current_tf)
+
+        self.go_to_position(target_tf)
+
+        return self
+    
+    def rot_z(self, theta: float):
+        _, current_tf = self.k.body_forward_kinematics(self.joint_angles)
+
+        target_tf = self.k.rot_z(theta, current_tf)
+
+        self.go_to_position(target_tf)
+
+        return self
+
 
 def main():
     robot = UR5Controller()
 
     robot.set_joint_angles(robot.DEFAULT_POSITIONS['HOME'])
     robot.go_to_position(robot.TEST_TARGETS['TEST'])
+    robot.update_joint_angles()
+    robot.k.body_forward_kinematics(robot.joint_angles)
     time.sleep(1)
-    robot.go_to_position(robot.TEST_TARGETS['TEST2'])
+    while True:
+        robot.move_y(200)
+        robot.move_y(-200)
+        robot.rot_y(pi/4)
+        robot.rot_y(-pi/4)
 
 if __name__ == '__main__':
     main()
