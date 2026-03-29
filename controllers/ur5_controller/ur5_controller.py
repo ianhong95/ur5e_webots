08115590591798c28319b0c895d137895c15721a
Ian Hong <ianqyhong@gmail.com>
2026-03-29 11:59:21 -0400

    Added joint speed control for smooth Cartesian movements, added PI control.

--- controllers/ur5_controller/ur5_controller.py
+++ controllers/ur5_controller/ur5_controller.py
@@ -7,7 +7,7 @@ from math import pi
 import numpy as np
 import time
 
-from ur5_definitions import Joint, IntConstants, FloatConstants, PhysicalParams
+from ur5_definitions import Joint, IntConstants, Thresholds, PhysicalParams, Tuning
 from kinematics import Kinematics
 
 
@@ -106,11 +106,11 @@ class UR5Controller(Robot):
         for i in range(IntConstants.MAX_ITERATIONS):
 
             # Set the initial guess to the "target_joint_angles" which are initially the current joint angles
-            delta_theta, rot_error, trans_error = self.k.inv_kinematics(target_tf, target_joint_angles)
+            delta_theta, rot_error, trans_error, twist_error_6D = self.k.inv_kinematics(target_tf, target_joint_angles)
 
-            if rot_error > FloatConstants.IK_ERROR_THRESHOLD or trans_error > FloatConstants.IK_ERROR_THRESHOLD:
+            if rot_error > Thresholds.IK_ERROR_THRESHOLD or trans_error > Thresholds.IK_ERROR_THRESHOLD:
                 for idx, angle_increment in enumerate(delta_theta):
-                    target_joint_angles[idx] += FloatConstants.DAMPING_FACTOR * angle_increment.item()
+                    target_joint_angles[idx] += Thresholds.DAMPING_FACTOR * angle_increment.item()
             else:
                 break
 
@@ -122,12 +122,51 @@ class UR5Controller(Robot):
             self.update_joint_angles()
             for current, target in zip(self.joint_angles, target_joint_angles):
                 error = target - current
-                if (abs(error)) > FloatConstants.THETA_THRESHOLD:
+                if (abs(error)) > Thresholds.THETA_THRESHOLD:
                     print(f'Target not reached yet. Error: {error}')
                     self.target_reached = False
 
             if self.target_reached:
                 return
+            
+    def go_to_speed(self, target_tf: np.ndarray):
+        """
+        Use inverse velocity kinematics to move the end-effector in a straight line.
+        """
+        for motor in self.motors.values():
+            motor.setPosition(float('inf'))
+            motor.setVelocity(0.0)
+
+        integral_error = np.zeros(6)
+        delta_t = self.TIMESTEP / 1000.0
+
+        while self.step(self.TIMESTEP) != -1:
+            self.update_joint_angles()
+
+            target_joint_angles = self.joint_angles.copy()
+
+            exp, T_sb = self.k.body_forward_kinematics(target_joint_angles)
+            current_jacobian = self.k.body_jacobian(exp)
+            twist_error = self.k.compute_twist_error(T_sb, target_tf)
+
+            twist_error_6D = self.k.se3_to_twist(twist_error, 'v')
+            rot_error = np.linalg.norm(twist_error_6D[:3])
+            trans_error = np.linalg.norm(twist_error_6D[3:6])
+
+            integral_error += twist_error_6D * delta_t
+
+            new_twist_error = (twist_error_6D * Tuning.K_P) + (integral_error * Tuning.K_I)
+
+            joint_velocities = np.linalg.pinv(current_jacobian) @ new_twist_error
+
+            if rot_error > Thresholds.ROT_ERROR_THRESHOLD or trans_error > Thresholds.TRANS_ERROR_THRESHOLD:
+                for joint, motor in self.motors.items():
+                    scalar_joint_velocity = np.clip(joint_velocities[joint.idx].reshape(()), -1, 1)
+                    motor.setVelocity(scalar_joint_velocity)
+            else:
+                for motor in self.motors.values():
+                    motor.setVelocity(0.0)
+                break
             
 
     def set_joint_angles(self, joint_angle_list: list[float]):
@@ -144,7 +183,7 @@ class UR5Controller(Robot):
 
                 joint_angle_error = abs(current_angle - joint_angle_list[i])
 
-                if joint_angle_error > FloatConstants.IK_ERROR_THRESHOLD:
+                if joint_angle_error > Thresholds.IK_ERROR_THRESHOLD:
                     print(f'Target not reached. Error: {joint_angle_error}')
                     self.target_reached = False
                     break
@@ -154,6 +193,21 @@ class UR5Controller(Robot):
             if self.target_reached:
                 print(f'Target reached!')
                 break
+        
+    # ================
+    # VELOCITY CONTROL
+    # ================
+
+    def set_joint_velocity(self, joint_velocity_list: list[float]):
+        """
+        Velocity control for all joints.
+
+        First we have to set the motor position to infinity, then set the joint velocity.
+        """
+
+        for joint, motor in self.motors.items():
+            motor.setPosition(float('inf'))
+            motor.setVelocity(joint_velocity_list[joint.idx])
 
     def move_x(self, x_target: float):
         target_coords = (x_target, 0, 0)
@@ -161,7 +215,8 @@ class UR5Controller(Robot):
 
         target_tf = self.k.rel_trans_xyz(target_coords, current_tf)
 
-        self.go_to_position(target_tf)
+        # self.go_to_position(target_tf)
+        self.go_to_speed(target_tf)
 
         return self
     
@@ -222,20 +277,20 @@ def main():
     robot.set_joint_angles(robot.DEFAULT_POSITIONS['HOME'])
     robot.go_to_position(robot.TEST_TARGETS['TEST'])
     _, fk = robot.k.body_forward_kinematics(robot.joint_angles)
-    print(f'fk: {fk}')
-    print(f'joint_angles: {robot.joint_angles}')    
+    # print(f'fk: {fk}')
+    # print(f'joint_angles: {robot.joint_angles}')    
     
     while True:
         robot.move_x(-150)
-        robot.move_y(200)
-        robot.move_x(150)
-        robot.move_y(-400)
-        robot.move_x(-150)
-        robot.move_z(150)
-        robot.move_y(200)
-        robot.move_z(-150)
-        robot.rot_y(pi/4)
-        robot.rot_y(-pi/4)
+        # robot.move_y(200)
+        # robot.move_x(150)
+        # robot.move_y(-400)
+        # robot.move_x(-150)
+        # robot.move_z(150)
+        # robot.move_y(200)
+        # robot.move_z(-150)
+        # robot.rot_y(pi/4)
+        # robot.rot_y(-pi/4)
         robot.move_x(150)
 
 if __name__ == '__main__':
