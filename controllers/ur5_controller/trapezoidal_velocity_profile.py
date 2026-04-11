8184187100164f43c084241c05ae7d93ecc43f67
Ian Hong <ianqyhong@gmail.com>
2026-04-11 15:00:52 -0400

    Rough reimplementation of trapezoidal velocity profile based on time elapsed.

--- controllers/ur5_controller/trapezoidal_velocity_profile.py
+++ controllers/ur5_controller/trapezoidal_velocity_profile.py
@@ -1,5 +1,6 @@
 import numpy as np
-from ur5_definitions import Joint, IntConstants, Thresholds, PhysicalParams, Tuning, MotionConstants
+from ur5_definitions import Joint, IntConstants, Thresholds, PhysicalParams, Tuning
+from ur5_definitions import MotionConstants as M
 from pid_helper import PID_Controller as pid
 
 class VelocityProfile():
@@ -14,25 +15,20 @@ class VelocityProfile():
         self.max_speed_reached = False     
         self.time = 0.0
         self.ramp_time = self.calc_ramp_time()
+        self.t_ramp_up = 0.0
+        self.t_cruise = 0.0
+        self.T = 0.0
 
-    def ramp_up(self, joint_velocities: np.ndarray, normalized_joint_velocities: np.ndarray):
+    def ramp_up(self):
         if not self.max_speed_reached:
             # v = v0 + (a * t) but capped at a max linear speed
-            self.current_speed = self.current_speed + (MotionConstants.LINEAR_ACCEL * (IntConstants.TIMESTEP / 1000.0))
-            new_joint_velocities = normalized_joint_velocities * self.current_speed
-
-            if self.current_speed >= MotionConstants.MAX_LINEAR_SPEED:
-                self.max_speed_reached = True
-        else:
-            new_joint_velocities = joint_velocities
-            
-        return new_joint_velocities
+            self.current_speed = self.current_speed + (M.LINEAR_ACCEL * (IntConstants.TIMESTEP / 1000.0))
     
     def calc_ramp_time(self):
         """
         The shortest time to go from 0 to max speed or from max speed to 0.
         """
-        t = MotionConstants.MAX_LINEAR_SPEED / MotionConstants.LINEAR_ACCEL
+        t = M.MAX_LINEAR_SPEED / M.MAX_LINEAR_ACCEL
 
         return t
     
@@ -46,25 +42,20 @@ class VelocityProfile():
         """
         Calculate the time required to reach the target if you move as fast as possible.
         """
-        term_1 = (dist_to_target / 1000.0) / MotionConstants.MAX_LINEAR_SPEED
-        term_2 = MotionConstants.MAX_LINEAR_SPEED / MotionConstants.LINEAR_ACCEL
+        term_1 = (dist_to_target / 1000.0) / M.MAX_LINEAR_SPEED
+        term_2 = M.MAX_LINEAR_SPEED / M.MAX_LINEAR_ACCEL
 
         T = term_1 + term_2
 
         return T
     
-    def ramp_down(self, joint_velocities: np.ndarray, normalized_joint_velocities: np.ndarray):
+    def ramp_down(self):
         if self.current_speed > 0:
             # v = v0 + (a * t)
-            self.current_speed = self.current_speed - (MotionConstants.LINEAR_ACCEL * (IntConstants.TIMESTEP / 1000.0))
-            new_joint_velocities = normalized_joint_velocities * self.current_speed
-        else:
-            new_joint_velocities = np.zeros(6)
-            
-        return new_joint_velocities
+            self.current_speed = self.current_speed - (M.MAX_LINEAR_ACCEL * (IntConstants.TIMESTEP / 1000.0))
     
     def calc_min_braking_distance(self):
-        return (self.current_speed ** 2) / (2 * MotionConstants.LINEAR_ACCEL)
+        return (self.current_speed ** 2) / (2 * M.MAX_LINEAR_ACCEL)
     
     def trapezoid(
             self,
@@ -83,3 +74,56 @@ class VelocityProfile():
             new_joint_velocities = self.ramp_down(joint_velocities, normalized_joint_velocities)
 
         return new_joint_velocities, self.current_speed
+    
+    def calc_lin_speed(self, current_tf: np.ndarray, target_tf: np.ndarray):
+        d = self.dist_to_target(current_tf, target_tf)
+        T = self.min_time_to_target(d)
+        mbt = self.calc_min_braking_distance()
+
+        if T > mbt:
+            self.ramp_up()
+        else:
+            self.ramp_down()
+
+        return self.current_speed
+    
+    def calc_time_markers(self):
+        self.t_ramp_up = M.MAX_LINEAR_SPEED / M.MAX_LINEAR_ACCEL
+        self.T = (M.MAX_LINEAR_ACCEL + (M.MAX_LINEAR_SPEED ** 2)) / (M.MAX_LINEAR_ACCEL * M.MAX_LINEAR_SPEED)
+        self.t_cruise = self.T - 2 * self.t_ramp_up
+    
+    def calc_s(self, t: int):
+        """
+        Calculate s(t) and s_dot(t), the normalized path parameters.
+
+        This is divided into 3 cases, one for each section of the trapezoid.
+
+        Returns s and s_dot as scalars from 0 to 1.
+        """
+
+        # dist_remaining = self.dist_to_target
+
+        # Case 1
+        if t >= 0 and t <= self.t_ramp_up:
+            s = 0.5 * M.MAX_LINEAR_ACCEL * t ** 2
+            s_dot = M.MAX_LINEAR_ACCEL * t
+        elif t > self.t_ramp_up and t <= (self.T - self.t_ramp_up):
+            s = M.MAX_LINEAR_SPEED * t - ((M.MAX_LINEAR_SPEED ** 2) / (2 * M.MAX_LINEAR_ACCEL))
+            s_dot = M.MAX_LINEAR_SPEED
+        elif t > (self.T - self.t_ramp_up) and t <= self.T:
+            # Split up terms for readability
+            a = 2 * M.MAX_LINEAR_ACCEL * M.MAX_LINEAR_SPEED * self.T
+            b = 2 * M.MAX_LINEAR_SPEED ** 2
+            c = (M.MAX_LINEAR_ACCEL ** 2) * (t - self.T) ** 2
+            d = 2 * M.MAX_LINEAR_ACCEL
+            s = (a - b - c) / d
+
+            s_dot = -M.MAX_LINEAR_ACCEL * (t - self.T)
+        else:
+            s = 0.0
+            s_dot = 0.0
+            print(f'Invalid normalized path case.')
+
+        return s, s_dot
+
+
