ab73039c444a2b347d4f8c1d094f0e193ee0e5c2
Ian Hong <ianqyhong@gmail.com>
2026-05-17 14:53:43 -0400

    Fixed keyboard logic.

--- controllers/keyboard_controller/keyboard_controller.py
+++ controllers/keyboard_controller/keyboard_controller.py
@@ -18,84 +18,103 @@ class KeyboardController(RobotMotion):
 
         self.move_state = MoveStates.STOPPED
         self.speed = 0.0    # mm/s
-        self.prev_keys = set()
+        self.prev_key = -1
+        self.held_key = -1
 
     def get_keys(self):
-        key = self.kb.getKey()
+        pressed_key = self.kb.getKey()
 
-        pressed_keys = set()
-
-        while key != -1:
-            pressed_keys.add(key)
-            key = self.kb.getKey()
-
-        return pressed_keys
+        return pressed_key
     
-    def get_state(self, prev_keys: set):
-        new_keys = self.get_keys()
-        held_keys = new_keys & prev_keys
-        released_keys = prev_keys - new_keys
+    def update_kb_state(self):
+        new_key = self.get_keys()
+        
+        # Key pressed for the first time
+        if new_key != -1 and self.prev_key == -1 and self.held_key == -1:
+            print(f'Key pressed: {new_key}')
+            self.prev_key = new_key
+            self.key_pressed(new_key)
+            return
 
-        for key in new_keys:
-            self.prev_keys.add(key)
+        # Key pressed is same as previously pressed key (held)
+        elif new_key != -1 and new_key == self.prev_key:
+            self.held_key = new_key
+            print(f'Holding key: {self.held_key}')
+            self.key_held(new_key)
+            return
 
-        return new_keys, held_keys, released_keys
+        # A key is pressed but is different from previous key
+        elif new_key != -1 and new_key != self.prev_key:
+            print(f'New key pressed: {new_key}')
+            self.key_released()
+            self.prev_key = new_key
+            self.key_pressed(new_key)
+            return
+
+        # Key is released
+        elif new_key == -1 and self.prev_key != -1:
+            print(f'Key released: {self.prev_key}')
+            self.key_released()
+            return
+
+        # No key is pressed
+        elif new_key == -1 and self.prev_key == -1:
+            return
     
     def key_pressed(self, key: int):
-        if self.move_state is MoveStates.STOPPED:
-            self.update_joint_angles()
-            _, T_sb = self.space_forward_kinematics(self.joint_angles)
+        # if self.move_state is MoveStates.STOPPED:
+        if self.is_moving:
+            self.controller.update_joint_angles()
+            _, T_sb = self.controller.space_forward_kinematics(self.controller.joint_angles)
 
         match key:
             case (KeyMap.FORWARD):
-                self.move_x(Constants.MOVE_STEP, T_sb)
+                self.move_rel(x=10.0)
             case (KeyMap.BACKWARD):
-                self.move_x(-Constants.MOVE_STEP, T_sb)
+                self.move_rel(x=-10.0)
             case (KeyMap.LEFT):
-                self.move_y(-Constants.MOVE_STEP, T_sb)
+                self.move_rel(y=10.0)
             case (KeyMap.RIGHT):
-                self.move_y(Constants.MOVE_STEP, T_sb)
+                self.move_rel(y=-10.0)
             case (KeyMap.UP):
-                self.move_z(Constants.MOVE_STEP, T_sb)
+                self.move_rel(z=10.0)
             case (KeyMap.DOWN):
-                self.move_z(-Constants.MOVE_STEP, T_sb)
+                self.move_rel(z=-10.0)
             case (KeyMap.CLOSE):
-                self.set_gripper(1.0)
+                self.controller.set_gripper(1.0)
             case (KeyMap.OPEN):
-                self.set_gripper(0.0)
+                self.controller.set_gripper(0.0)
+            case (KeyMap.HOME):
+                self.move_abs(Positions.HOME)
 
         self.move_state == MoveStates.MOVING
 
-    def key_held(self, key: set):
-        self.update_joint_angles()
-        _, T_sb = self.space_forward_kinematics(self.joint_angles)
+    def key_held(self, key):
+        self.controller.update_joint_angles()
+        _, T_sb = self.controller.space_forward_kinematics(self.controller.joint_angles)
 
         match key:
             case (KeyMap.FORWARD):
-                self.move_x(Constants.MOVE_STEP, T_sb)
+                self.move_rel(x=10.0)
             case (KeyMap.BACKWARD):
-                self.move_x(-Constants.MOVE_STEP, T_sb)
+                self.move_rel(x=-10.0)
             case (KeyMap.LEFT):
-                self.move_y(-Constants.MOVE_STEP, T_sb)
+                self.move_rel(y=10.0)
             case (KeyMap.RIGHT):
-                self.move_y(Constants.MOVE_STEP, T_sb)
+                self.move_rel(y=-10.0)
             case (KeyMap.UP):
-                self.move_z(Constants.MOVE_STEP, T_sb)
+                self.move_rel(z=10.0)
             case (KeyMap.DOWN):
-                self.move_z(-Constants.MOVE_STEP, T_sb)
+                self.move_rel(z=-10.0)
             case (KeyMap.CLOSE):
-                self.set_gripper(1.0)
+                self.controller.set_gripper(1.0)
             case (KeyMap.OPEN):
-                self.set_gripper(0.0)
+                self.controller.set_gripper(0.0)
 
     def key_released(self):
-        self.move_state = MoveStates.STOPPED
-
-    def go_to_home(self):
-        self.controller.moveL(Positions.HOME)
-
-        while self.step(self.controller.TIMESTEP) != -1:
-            if not self.target_reached:
-                self.controller.moveL(Positions.HOME)
-            elif self.target_reached:
-                print(f'Moved to home')
+        self.is_moving = False
+        self.held_key = -1
+        self.prev_key = -1
+        self.reset()
+        self.queue.clear()
+        self.controller.reset_motor_speeds()
