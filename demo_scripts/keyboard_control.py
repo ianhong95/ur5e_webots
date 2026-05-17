2daeb8e88b0ab0cf2543ac533efdafbb0b18d9a2
Ian Hong <ianqyhong@gmail.com>
2026-05-17 14:55:36 -0400

    Added keyboard support.

--- demo_scripts/keyboard_control.py
+++ demo_scripts/keyboard_control.py
@@ -21,17 +21,4 @@ def run():
     while kb.controller.step(kb.controller.TIMESTEP) != -1:
         kb.update_state(kb.controller.TIMESTEP / 1000.0) 
 
-    # new_keys, held_keys, released_keys = controller.get_state(controller.prev_keys)
-
-    # if new_keys:
-    #     for key in new_keys:
-    #         if key in held_keys:
-    #             print(f'key_held! {key}')
-    #             controller.key_held(key)
-    #         elif key in released_keys:
-    #             print(f'key_released! {key}')
-    #             controller.key_released()
-    #         else:
-    #             controller.key_pressed(key)
-    #             print(f'key pressed: {key}')
-    #             controller.moving = False
+        kb.update_kb_state()
